06cc66de60da2933e7a7c183c46c872130081c29
Saverio Negro <saverionegro@gmail.com>
2024-11-23 12:10:42 +0100

    Create Project

---
+++ Multably/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Multably
+//
+//  Created by Saverio Negro on 23/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Multably/MultablyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultablyApp.swift
+//  Multably
+//
+//  Created by Saverio Negro on 23/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MultablyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
