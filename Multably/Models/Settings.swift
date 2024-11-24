b44418cb58d75943acc6da3425579053936d5938
Saverio Negro <saverionegro@gmail.com>
2024-11-24 20:44:29 +0100

    Create `Settings` class for keeping track of data relative to user's settings.

---
+++ Multably/Models/Settings.swift
@@ -0,0 +1,13 @@
+//
+//  Settings.swift
+//  Multably
+//
+//  Created by Saverio Negro on 24/11/24.
+//
+import SwiftUI
+
+@Observable
+class Settings {
+    var numQuestions: Int = 5
+    var multiplicationTable: Int = 2
+}
