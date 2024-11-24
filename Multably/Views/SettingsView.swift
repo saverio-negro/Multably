386a308f5f4f90f22498f648776c63b15623cebb
Saverio Negro <saverionegro@gmail.com>
2024-11-24 20:42:05 +0100

    Create `SettingsView` to let users change game's settings.

--- Multably/Views/SettingsView.swift
+++ Multably/Views/SettingsView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct SettingsView: View {
+    
+    let numQuestions = [5, 10, 20]
+    @State private var selectedNumQuestions = 5
+    @State private var selectedTable = 2
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Form {
+            
+            Section("Multiplication Table") {
+                Stepper("\(selectedTable) Times Table", value: $selectedTable, in: 2...12)
+            }
+            
+            Section("Number of questions") {
+                Picker("Number of questions", selection: $selectedNumQuestions) {
+                    ForEach(numQuestions, id: \.self) { num in
+                        Text("\(num)")
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+        }
     }
 }
 
